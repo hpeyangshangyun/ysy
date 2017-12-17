777bf34f51e37f5359e2b4c05357e0bb433b4af8
e <hpe_cd_ysy@163.com>
2017-12-17 23:13:09 +0800

    首页的制作

---
+++ js/base.js
@@ -0,0 +1,13 @@
+//页面加载时，加载
+window.onload=function(){
+	
+}
+
+
+
+//jquery正则表达式解析url，通过url获取参数
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
