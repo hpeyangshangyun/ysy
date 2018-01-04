00ca079cee1f39188057cd636fa34f945e6adeeb
e <hpe_cd_ysy@163.com>
2018-01-04 22:25:10 +0800

    导航栏的收起与展开

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
 
+
+window.onload=function(){
+	rightShow("./web/mine/noForget.html")
+}
+
+var config={
+	leftNav: true,//左侧导航栏是否显示，默认为true，显示
+}
+
 // 左侧导航栏的收起与展开
-function leftNav(){
-	
+function leftNav(item){
+	console.log($(item).html())
+	if(config.leftNav==true){
+		// $("#leftNav").addClass("hidden");
+		$("#leftNav .col-9").css("width",0)
+		$("#leftNav").css("width","2%")
+		$("#rightShow").css("width","98%");
+		$(item).empty();
+		$(item).html("<span>&#9758;</span>");//箭头向右
+		$(item).parent().attr("title","点击手型，展开导航栏")
+		config.leftNav=false;
+	}else{
+		$("#leftNav .col-9").css("width","90%");
+		$("#leftNav").css("width","20%")
+		$("#rightShow").css("width","80%");
+		$(item).empty();
+		$(item).html("<span>&#9756;</span>");//箭头向左
+		$(item).parent().attr("title","点击手型，收回导航栏")
+		config.leftNav=true;
+	}
 }
 // 根据左侧的选择进行右侧显示区域的显示
 function rightShow(url){
-	$("#rightShow").load('./web/mine/noForget.html');
+	$("#rightShow").load(url);
 }
