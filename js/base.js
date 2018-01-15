b00a644f9752f07c7670aaa648b749f849c2b0ad
e <hpe_cd_ysy@163.com>
2018-01-15 14:08:47 +0800

    js闭包

--- js/base.js
+++ js/base.js
@@ -3,7 +3,24 @@ window.onload=function(){
 	
 }
 
-
+function Post(url,json,type,callback,async){
+	type=="post"?"post":"get";
+	async==false? false:true;
+	$.ajax({
+		cache: false,//缓存
+		type: type,
+		url:url,
+		data:json,
+		dataType: "json",
+		async: async,//同步还是异步
+		success: function(data){
+			callback(data);
+		},
+		error: function(data){
+			callback(data);
+		}
+	})
+}
 
 //jquery正则表达式解析url，通过url获取参数
 function getQueryString(name) {
